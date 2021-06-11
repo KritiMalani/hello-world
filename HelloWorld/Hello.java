17728970dceb14446c13761b7d6ff988d3d2125a
KritiMalani <85431397+KritiMalani@users.noreply.github.com>
2021-06-11 15:37:48 +0530

    Add files via upload

---
+++ HelloWorld/Hello.java
@@ -0,0 +1,16 @@
+
+/**
+ * this is my first assignment in JAVA
+ *
+ ** @Kriti (your name)
+ * @1.0 (a version number or a date)
+ */
+
+public class Hello
+{
+    public static void main(String[] args) {
+        //TODO code application logic here
+        System.out.println("Hello!");
+    }
+    
+}
